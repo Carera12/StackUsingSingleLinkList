fd036824496412b47eebb783654581044c7292e7
M S I <M S I@MSI>
2022-11-28 17:23:13 +0700

    add public void display()

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -45,6 +45,19 @@ namespace StackUsingSingleLinkList
             Console.WriteLine("\n the poped elements is: " + top.info);
             top = top.next; // make top point to thr nest node is squence
         }
+        public void display()
+        {
+            node tmp;
+            if (empty())
+                Console.WriteLine("\n Stack Empty");
+            else
+            {
+                for (tmp = top; tmp !=null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+            }
+        }
     }
     class Program
     {
