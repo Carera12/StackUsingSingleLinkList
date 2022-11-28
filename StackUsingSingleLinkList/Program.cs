96e8b9d9b60f99f4294d1db2e9eed8209623edc9
M S I <M S I@MSI>
2022-11-28 17:26:11 +0700

    add Stact Menu

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -52,17 +52,27 @@ namespace StackUsingSingleLinkList
                 Console.WriteLine("\n Stack Empty");
             else
             {
-                for (tmp = top; tmp !=null; tmp = tmp.next)
+                for (tmp = top; tmp != null; tmp = tmp.next)
                 {
                     Console.WriteLine(tmp.info);
                 }
             }
         }
-    }
-    class Program
-    {
+
         static void Main(string[] args)
         {
+            Stack s = new Stack();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n*****Stack Menu*****");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("\nEnter your Choice: ");
+            }
         }
+
     }
 }
